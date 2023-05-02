372d60fe78558445adcceef05901e9e66bc940aa
Excellentc <kochapp@gmail.com>
2023-05-02 15:57:52 +0300

    function for check radio buttons - two from three

---
+++ lecture_selenium/src/pages/page_radiobutton.py
@@ -0,0 +1,46 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class RadioButton:
+    def __init__(self, driver: WebDriver, name: None):
+        self.driver = driver
+        self.locator = '//label[.="{}"]//ancestor::div[contains(@class, "radio")]'.format(name)
+        self.input_ = '/input'
+        self.label = '/label'
+
+    def is_selected(self) -> bool:
+        element = self.driver.find_element(By.XPATH, f'{self.locator}{self.input_}')
+        return element.is_selected()
+
+    def select(self) -> None:
+        self.driver.find_element(By.XPATH, f'{self.locator}{self.label}').click()
+
+    def enable(self):
+        locator = (By.XPATH, f'{self.locator}{self.input_}')
+        element = self.driver.find_element(*locator)
+
+        if not element.is_enabled():
+            self.driver.execute_script("arguments[0].disabled = false;", element)
+        return self
+
+    # class ListRadioButton:
+    #     def __init__(self, driver: WebDriver):
+    #         self.driver = driver
+    def list_of_buttons(self):
+        radio_button_elements = self.driver.find_elements(By.XPATH, '//div[contains(@class, "radio")]')
+        radio_button_status = {}
+
+        for element in radio_button_elements:
+            name_element = element.find_element(By.TAG_NAME, 'label')
+            name = name_element.text.strip()
+
+            input_element = element.find_element(By.TAG_NAME, 'input')
+            if input_element.is_selected():
+                radio_button_status[name] = 'Active'
+            elif input_element.is_enabled():
+                radio_button_status[name] = 'Enabled'
+            else:
+                radio_button_status[name] = 'Off'
+
+        return radio_button_status
---
+++ lecture_selenium/src/tests/test_radio_buttons.py
@@ -0,0 +1,48 @@
+import pytest
+from lecture_selenium.src.pages.page_radiobutton import RadioButton
+# from lecture_selenium.src.pages.page_radiobutton import ListRadioButton
+
+
+@pytest.mark.usefixtures('chrome')
+class TestRadioButton:
+    def test_activate_yes_radio(self):
+        self.driver.get('https://demoqa.com/radio-button')
+        radiobutton_yes = RadioButton(self.driver, name='Yes')
+        radiobutton_yes.enable().select()
+        status_button_yes = radiobutton_yes.is_selected()
+
+        radiobutton_impressive = RadioButton(self.driver, name='Impressive')
+        radiobutton_no = RadioButton(self.driver, name='No')
+
+        status_button_impressive = radiobutton_impressive.is_selected()
+        status_button_no = radiobutton_no.is_selected()
+
+        assert status_button_yes is True and (
+                status_button_no is False and status_button_impressive is False), "Radio button YES is OFF"
+        assert status_button_yes is True, "Radio button YES is OFF"
+
+    def test_activate_disabled_radio_button(self):
+        self.driver.get('https://demoqa.com/radio-button')
+        radiobutton_no = RadioButton(self.driver, name='No')
+        radiobutton_no.enable().select()
+        status_button_no = radiobutton_no.is_selected()
+
+        radiobutton_impressive = RadioButton(self.driver, name='Impressive')
+        radiobutton_yes = RadioButton(self.driver, name='Yes')
+
+        status_button_impressive = radiobutton_impressive.is_selected()
+        status_button_yes = radiobutton_yes.is_selected()
+
+        assert status_button_no is True and (
+                status_button_yes is False and status_button_impressive is False), "Radio button NO is OFF"
+
+    #
+    # @pytest.mark.usefixtures('chrome')
+    # class TestListRadioButton:
+    def test_get_radio_buttons_info(self):
+        self.driver.get('https://demoqa.com/radio-button')
+        radio_button_list = RadioButton(self.driver, name=None)
+        radio_button_list.list_of_buttons()
+        print(radio_button_list)
+        # for name, status in radio_button_status.items():
+        #     print(f"Кнопка '{name}': {status}")
