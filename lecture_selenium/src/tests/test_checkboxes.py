0ce249165656b218d62a336f8f31150f73ba87bc
Excellentc <kochapp@gmail.com>
2023-05-02 18:37:39 +0300

    add short  # commit for def test_checkbox_task_one

--- lecture_selenium/src/tests/test_checkboxes.py
+++ lecture_selenium/src/tests/test_checkboxes.py
@@ -10,14 +10,14 @@ class TestCheckbox:
         self.driver: WebDriver = self.driver
         self.page = TestTextCheckbox(self.driver).open()
 
-    def test_checkbox_task_one(self):
+    def test_checkbox_task_one(self):                   # for one function ver.2
         self.page.expand_folder('home')
         self.page.expand_folder('documents', parent='home')
-        # self.page.expand_second_folder('documents')    ----------for two function , first  version
+        # self.page.expand_second_folder('documents')    for two function ,   ver.1
         assert self.page.mark_folder('desktop')
         assert self.page.mark_folder('office')
 
-    def test_checkbox_task_one_for_list(self):
+    def test_checkbox_task_one_for_list(self):          # for one function ver.3
         checkbox_list = ['home', 'documents']
         self.page.expand_folder_from_list(names=checkbox_list)
         assert self.page.mark_folder('desktop')
