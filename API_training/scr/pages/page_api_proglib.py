11a3dd96881729ae904ff3b2e44d3befb3c4bb63
Excellentc <kochapp@gmail.com>
2023-05-30 19:41:39 +0300

    add test_api_page.py

--- API_training/scr/pages/page_api_proglib.py
+++ API_training/scr/pages/page_api_proglib.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 import requests
 from selenium.webdriver.remote.webdriver import WebDriver
@@ -12,9 +13,13 @@ class ApiPage:
     @staticmethod
     def get_api_users_list(list_of_users_url=None):
         response_users = requests.get(list_of_users_url)
+        with open('users_list.json', 'w') as file:
+            json.dump(response_users.json(), file, indent=4)
         return response_users
 
     @staticmethod
     def post_new_user(add_user_url, user_data: tuple):
         response_add_user = requests.post(add_user_url, data=user_data)
+        with open('registration_new_user.json', 'w') as file:
+            json.dump(response_add_user.json(), file, indent=4)
         return response_add_user
